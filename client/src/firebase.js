35f14b198a04cb93a967778762ff85289a9f22dc
Duy Nguyen <nguyenduy574@outlook.com>
2024-05-09 19:40:40 +0700

    update firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,14 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore, collection } from 'firebase/firestore'
-// ... other firebase imports
+import { getFirestore } from 'firebase/firestore'
+
 const firebaseConfig = {
- apiKey: "AIzaSyBsjlTTcrvCSgS0YhHxz9LLhyHTQ2lxtSg",
- authDomain: "wemis-dxa.firebaseapp.com",
- databaseURL: "https://wemis-dxa-default-rtdb.asia-southeast1.firebasedatabase.app",
- projectId: "wemis-dxa",
- storageBucket: "wemis-dxa.appspot.com",
- messagingSenderId: "1029877148537",
- appId: "1:1029877148537:web:3eb1a5155f639a08f1dda8",
- measurementId: "G-9YQJMTG53Q"
+ apiKey: "AIzaSyAuucjlB-Mi2qo8Ujig9qKuw__QG0dANUs",
+ authDomain: "wewallet-weelearn.firebaseapp.com",
+ projectId: "wewallet-weelearn",
+ storageBucket: "wewallet-weelearn.appspot.com",
+ messagingSenderId: "743313162945",
+ appId: "1:743313162945:web:93d6ca41724b335c4dca40",
+ measurementId: "G-15B2Q6W9CS"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig)
-
-// used for the firestore refs
-const db = getFirestore(firebaseApp)
-
-// here we can export reusable database references
-// export const todosRef = collection(db, 'expense')
