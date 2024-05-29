eb2eff54700ef0dc732db91d5a6f2882e3da5737
Duy Nguyen <nguyenduy574@outlook.com>
2024-05-29 19:08:03 +0700

    s

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyAuucjlB-Mi2qo8Ujig9qKuw__QG0dANUs",
- authDomain: "wewallet-weelearn.firebaseapp.com",
- projectId: "wewallet-weelearn",
- storageBucket: "wewallet-weelearn.appspot.com",
- messagingSenderId: "743313162945",
- appId: "1:743313162945:web:93d6ca41724b335c4dca40",
- measurementId: "G-15B2Q6W9CS"
+  apiKey: "AIzaSyAuucjlB-Mi2qo8Ujig9qKuw__QG0dANUs",
+  authDomain: "wewallet-weelearn.firebaseapp.com",
+  projectId: "wewallet-weelearn",
+  storageBucket: "wewallet-weelearn.appspot.com",
+  messagingSenderId: "743313162945",
+  appId: "1:743313162945:web:93d6ca41724b335c4dca40",
+  measurementId: "G-15B2Q6W9CS",
 };
 
-export const firebaseApp = initializeApp(firebaseConfig)
+export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
